f362c7a055ab55b51f592070f48177654581c4ec
azemazer <a.laval16@laposte.net>
2024-01-29 10:08:10 +0100

    Init work done: basic homepage without style

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [prenom, setPrenom] = useState("");
+
+  const assignPrenom = (e) => {
+    setPrenom(e.target.value);
+  }
+
+  const submitPrenom = () => {
+    if (!prenom || prenom === "") {
+      return alert("Il faut rentrer un prénom!!!!");
+    }
+    alert("Bonjour, " + prenom + " !");
+    setPrenom("");
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Bienvenue chez nous !</h1>
+      <h3>Connectez-vous</h3>
+      <form>
+        <input type="text" name="prenom" placeholder="Entrez votre prénom" value={prenom} onChange={(e) => assignPrenom(e)}></input>
+      </form>
+      <button onClick={() => submitPrenom()}> Accèdez à votre espace </button>
     </>
   )
 }
